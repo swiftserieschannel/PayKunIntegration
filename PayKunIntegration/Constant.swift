67fee336f2efd407b719467cbec8c6e04570e945
Chander bhushan <cbhushan1995@gmail.com>
2019-10-03 13:46:59 +0530

    PayKun Integration
    Sample app for PayKun integration!

---
+++ PayKunIntegration/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  PayKunIntegration
+//
+//  Created by chander bhushan on 03/10/19.
+//  Copyright © 2019 swiftseries. All rights reserved.
+//
+
+import Foundation
+
+struct Constents {
+    static public var API_KEY = "PLACE_YOUR_API_KEY"
+    static public var MERCHANT_KEY = "PLACE_YOUR_MERCHANT_KEY"
+}
--- PayKunIntegration/ViewController.swift
+++ PayKunIntegration/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
-
+import PayKun
 class ViewController: UIViewController {
 
+    var objPaykun: PaykunCheckout!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        objPaykun = PaykunCheckout.init(key: Constents.API_KEY, merchantId: Constents.MERCHANT_KEY, isLive: true, andDelegate: self)
     }
 
+    
+    @IBAction func clickedBtnPay(_ sender: Any) {
+        let seconds = Int(NSDate().timeIntervalSince1970)
+        objPaykun.checkout(withCustomerName: "Sam", customerEmail: "hrsame@gmail.in", customerMobile: "88998787890", productName: "MAC IN", orderNo: String(seconds), amount: "12", viewController: self)
+    }
+    
+    
+}
 
+extension ViewController : PaykunCheckoutDelegate {
+    func onPaymentFailed(_ responce: [AnyHashable : Any]) {
+        debugPrint(responce)
+    }
+    
+    func onPaymentSucceed(_ responce: [AnyHashable : Any]) {
+        debugPrint(responce)
+    }
+    
+    
 }
 
